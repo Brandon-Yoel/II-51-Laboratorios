6fd16e934c57f0bf52c3801b01af464e1a76a151
Brandon-Yoel <brandonyel77@gmail.com>
2025-11-26 10:45:33 -0600

    Se completa el LAB08

---
+++ LAB08/script.js
@@ -0,0 +1,5 @@
+let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpbXh2eW5xcmVncHpwZ3Z5YWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MDkxMjYsImV4cCI6MjA3OTM4NTEyNn0.SLuYczImg9ZKeiafAI6rgr0-QsqEyG8EC7x-xEQW5vE";
+
+let supabaseUrl = "https://limxvynqregpzpgvyahg.supabase.co";
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
